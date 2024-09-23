aaa1a524a4b533430929a03ec51fe113e765e497
Amine Bouyarmane <amine.bouyarmane@gmail.com>
2024-09-23 20:11:03 +0200

    fix: depth strength

--- src/kinesisTransformerElement.ts
+++ src/kinesisTransformerElement.ts
@@ -82,17 +82,13 @@ class KinesisTransformerElement {
       case "tilt": {
         const rotateY = axis.includes("X") ? y * strength : 0;
         const rotateX = axis.includes("Y") ? x * strength : 0;
-        transformValue = `rotateX(${rotateY}deg) rotateY(${-rotateX}deg) translate3d(0,0,${
-          strength * 2
-        }px)`;
+        transformValue = `rotateX(${rotateY}deg) rotateY(${-rotateX}deg) translate3d(0,0,${strength}px)`;
         break;
       }
       case "tilt_inv": {
         const rotateY = axis.includes("X") ? y * strength : 0;
         const rotateX = axis.includes("Y") ? x * strength : 0;
-        transformValue = `rotateX(${-rotateY}deg) rotateY(${rotateX}deg) translate3d(0,0,${
-          strength * 2
-        }px)`;
+        transformValue = `rotateX(${-rotateY}deg) rotateY(${rotateX}deg) translate3d(0,0,${strength}px)`;
         break;
       }
     }
