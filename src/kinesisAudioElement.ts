9555545d7bae55318f29c890097868d3d6901374
Amine Bouyarmane <amine.bouyarmane@gmail.com>
2024-09-24 19:44:33 +0200

    feat: add axis to audio element

--- src/kinesisAudioElement.ts
+++ src/kinesisAudioElement.ts
@@ -1,12 +1,14 @@
 // kinesisAudioElement.ts
 
-import { TransformType } from "./types";
+import { TransformType, AxisType } from "./types";
+import { parseAxes } from "./utils";
 
 class KinesisAudioElement {
   element: HTMLElement;
   audioIndex: number;
   strength: number;
   type: TransformType;
+  axes: AxisType[];
   transformOrigin: string;
   initialTransform: string;
 
@@ -32,6 +34,19 @@ class KinesisAudioElement {
     this.transformOrigin =
       element.getAttribute("data-ks-transformorigin") || "center";
 
+    // Parse data-ks-axis attribute
+    const axisAttr = element.getAttribute("data-ks-axis");
+    if (axisAttr) {
+      this.axes = parseAxes(axisAttr);
+      if (this.axes.length === 0) {
+        // If parsing results in no valid axes, fallback to defaults
+        this.axes = this.getDefaultAxes();
+      }
+    } else {
+      // Set default axes based on transform type
+      this.axes = this.getDefaultAxes();
+    }
+
     const computedStyle = window.getComputedStyle(this.element);
     this.initialTransform =
       computedStyle.transform === "none" ? "" : computedStyle.transform;
@@ -42,31 +57,71 @@ class KinesisAudioElement {
     // Initialize CSS variables for dynamic transition durations
     this.element.style.setProperty("--transform-duration", "0.1s");
     this.element.style.transition = `transform var(--transform-duration) ease-out`;
+
+    // Improve performance by hinting the browser
+    this.element.style.willChange = "transform";
+  }
+
+  /**
+   * Returns default axes based on the TransformType
+   */
+  private getDefaultAxes(): AxisType[] {
+    switch (this.type) {
+      case "translate":
+        return ["Y"];
+      case "rotate":
+        return ["Z"];
+      case "scale":
+      default:
+        return ["X", "Y"];
+    }
   }
 
   applyTransform(value: number) {
-    let transformValue = "";
+    const { strength, type, axes } = this;
 
-    const { strength, type } = this;
+    const transforms: string[] = [];
 
-    switch (type) {
-      case "translate": {
-        const translateX = value * strength;
-        const translateY = value * strength;
-        transformValue = `translate(${translateX}px, ${translateY}px)`;
-        break;
+    axes.forEach((axis) => {
+      switch (type) {
+        case "translate": {
+          if (axis === "X" || axis === "Y" || axis === "Z") {
+            const translate = value * strength;
+            // For translate, set the axis being transformed and others to 0
+            const translateX = axis === "X" ? translate : 0;
+            const translateY = axis === "Y" ? translate : 0;
+            const translateZ = axis === "Z" ? translate : 0;
+            transforms.push(
+              `translate3d(${translateX}px, ${translateY}px, ${translateZ}px)`
+            );
+          }
+          break;
+        }
+        case "rotate": {
+          if (axis === "X" || axis === "Y" || axis === "Z") {
+            const rotate = value * strength;
+            // Rotate around the specified axis
+            transforms.push(`rotate${axis}(${rotate}deg)`);
+          }
+          break;
+        }
+        case "scale": {
+          if (axis === "X" || axis === "Y" || axis === "Z") {
+            // Scaling factors for x, y, z
+            const scale = 1 + value * strength * 0.1;
+            const scaleX = axis === "X" ? scale : 1;
+            const scaleY = axis === "Y" ? scale : 1;
+            const scaleZ = axis === "Z" ? scale : 1;
+            transforms.push(`scale3d(${scaleX}, ${scaleY}, ${scaleZ})`);
+          }
+          break;
+        }
+        default:
+          break;
       }
-      case "rotate": {
-        const rotate = value * strength;
-        transformValue = `rotate(${rotate}deg)`;
-        break;
-      }
-      case "scale": {
-        const scale = 1 + value * strength * 0.1;
-        transformValue = `scale(${scale})`;
-        break;
-      }
-    }
+    });
+
+    const transformValue = transforms.join(" ");
 
     // Determine if the frequency is increasing or decreasing
     const isRising = value > this.previousValue;
