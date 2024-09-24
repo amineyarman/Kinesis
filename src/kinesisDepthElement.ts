c028a1d42dbc93107312cbda740ab00eb5527a68
Amine Bouyarmane <amine.bouyarmane@gmail.com>
2024-09-24 17:49:30 +0200

    fix depth transition

--- src/kinesisDepthElement.ts
+++ src/kinesisDepthElement.ts
@@ -17,16 +17,19 @@ class KinesisDepthElement {
     this.initialTransform =
       computedStyle.transform === "none" ? "" : computedStyle.transform;
 
-    this.element.style.transform = `${this.initialTransform} translateZ(${this.depth}px)`;
+    // Initially, set the element at Z=0
+    this.element.style.transform = `${this.initialTransform} translateZ(0px)`;
     this.element.style.transition = `transform 1s cubic-bezier(0.23, 1, 0.32, 1)`;
   }
 
   applyDepth(newDepth: number) {
+    // When hovered, apply the depth defined by the user
     this.element.style.transform = `${this.initialTransform} translateZ(${newDepth}px)`;
   }
 
   resetDepth() {
-    this.element.style.transform = `${this.initialTransform} translateZ(${this.depth}px)`;
+    // On mouse leave, return to Z=0
+    this.element.style.transform = `${this.initialTransform} translateZ(0px)`;
   }
 }
 
